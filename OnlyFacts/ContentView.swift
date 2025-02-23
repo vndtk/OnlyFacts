de997e1bff1df5ccd386f3c4b88002d996a2195d
vndtk <vndtk@pm.me>
2025-02-23 03:59:18 -0600

    Initial Commit

---
+++ OnlyFacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlyFacts
+//
+//  Created by Vlady Schmidt on 2/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlyFacts/OnlyFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlyFactsApp.swift
+//  OnlyFacts
+//
+//  Created by Vlady Schmidt on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct OnlyFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
